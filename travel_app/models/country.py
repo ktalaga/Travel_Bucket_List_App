0988a5c13332de740a9d1db916e220da670bc721
konrad <ktalaga90@gmail.com>
2021-04-02 13:48:30 +0100

    added another 2 tests for country_test.py, both passed

--- travel_app/models/country.py
+++ travel_app/models/country.py
@@ -4,3 +4,6 @@ class Country:
         self.name = name
         self.visited = visited
         self.id = id
+
+    def mark_visited(self):
+        self.visited = True
--- travel_app/tests/country_test.py
+++ travel_app/tests/country_test.py
@@ -8,3 +8,10 @@ class TestCountry(unittest.TestCase):
 
     def test_country_has_name(self):
         self.assertEqual("Scotland", self.country.name)
+
+    def test_country_visited_starts_false(self):
+        self.assertEqual(False, self.country.visited)
+
+    def test_can_mark_country_visited(self):
+        self.country.mark_visited()
+        self.assertEqual(True, self.country.visited)
