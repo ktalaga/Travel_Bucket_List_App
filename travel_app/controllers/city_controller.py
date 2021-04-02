5cfb99c02f7de5573f32d1969daea04abf1056b9
konrad <ktalaga90@gmail.com>
2021-04-02 19:16:36 +0100

    created new template for add_new_city, provided link in the base.html, added route and rendered empty page. All works

--- travel_app/controllers/city_controller.py
+++ travel_app/controllers/city_controller.py
@@ -11,3 +11,7 @@ cities_blueprint = Blueprint("cities", __name__)
 def cities():
     cities = city_repository.select_all()
     return render_template("cities/index.html", cities = cities)
+
+@cities_blueprint.route("/addcity")
+def addcity():
+    return render_template("cities/addcity.html")
